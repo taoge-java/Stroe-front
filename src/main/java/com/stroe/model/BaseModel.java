bd34f057f4d2c840d77d30e9a894b637744ce4b0
zengjintao <Administrator@SC-201706212248>
2017-07-19 16:01:43 +0800

    。

--- src/main/java/com/stroe/model/BaseModel.java
+++ src/main/java/com/stroe/model/BaseModel.java
@@ -15,9 +15,6 @@ import com.jfinal.plugin.activerecord.Model;
 @SuppressWarnings("rawtypes")
 public class BaseModel <M extends Model> extends Model<M>{
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
     public void getParamters(Map<String,String[]> map){
